21b26f2080d8d8cf1cbb893de6a191bc2fcc65ab
DrewGithubS <chessboydrew@gmail.com>
2023-03-18 17:12:41 -0700

    Saving

--- main/NeuralNet/NeuralNetCUDA.cu
+++ main/NeuralNet/NeuralNetCUDA.cu
@@ -34,16 +34,71 @@ __global__ void d_randomizeNeurons(curandState * curandStates,
 	}
 }
 
+/*
+          zyx
+0  1  2   000 001 002
+3  4  5   010 011 012
+6  7  8   020 021 022
+
+9  10 11  100 101 102
+12 13 14  110 111 112
+15 16 17  120 121 122
+
+18 19 20  200 201 202
+21 22 23  210 211 212
+24 25 26  220 221 222
+
+x = (n % 3)
+y = ((n / 3) % 3)
+z = (n / (3 * 3))
+*/
+
 __global__ void d_createRandomConnections(curandState * curandStates,
-								   		  float minValue,
-								   		  float maxValue,
 								   		  int partitions,
-								   		  int connections){
+								   		  int neuronsPerPartition,
+								   		  int neurons,
+								   		  int connectionsPerNeuron,
+								   		  int connections) {
 
 	int index = threadIdx.x + blockDim.x*blockIdx.x;
 
 	if(index < connections) {
+		int neuronIndex = index / connectionsPerNeuron;
+		int partitionIndex = neuronIndex / neuronsPerPartition;
+		int partitionX = (partitionIndex % partitions); 
+		int partitionY = (partitionIndex / partitions) % partitions;
+		int partitionZ = (partitionIndex / (partitions * partitions);
 		
+		float minValueX = partitionX == 0 ? 0 : -1;
+		float minValueY = partitionY == 0 ? 0 : -1;
+		float minValueZ = partitionZ == 0 ? 0 : -1;
+
+		float maxValueX = partitionX == (partitions - 1) ? 0 : 1;
+		float maxValueY = partitionY == (partitions - 1) ? 0 : 1;
+		float maxValueZ = partitionZ == (partitions - 1) ? 0 : 1;
+
+		
+
+		float x_f = curand_uniform(my_curandstate + index);
+	    x_f *= (maxValueX - minValueX + 0.999999);
+	    x_f -= 1;
+	    dx = (int) truncf(x_f);
+
+	    float y_f = curand_uniform(my_curandstate + index);
+	    y_f *= (maxValueX - minValueX + 0.999999);
+	    y_f -= 1;
+	    dy = (int) truncf(y_f);
+
+	    float z_f = curand_uniform(my_curandstate + index);
+	    z_f *= (maxValueX - minValueX + 0.999999);
+	    z_f -= 1;
+	    dz = (int) truncf(z_f);
+
+
+
+		while() {
+
+		}
 	}
 }
 
@@ -79,9 +134,8 @@ void createRandomConnections(curandState * curandStates,
 							 int neuronsPerPartition,
 							 int maxConnectionsPerNeuron) {
 
-	int connections = partitionCount *
-		neuronsPerPartition *
-		maxConnectionsPerNeuron;
+	int neurons = partitionCount * neuronsPerPartition;
+	int connections = neurons * maxConnectionsPerNeuron;
 
 	// d_createRandomConnections <<< 
 	// 							  BlockCount(connections),
